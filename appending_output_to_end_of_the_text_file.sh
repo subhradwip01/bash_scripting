0f8b42c2410a801df1c5f90704ac950820ca63c3
subhradwip01 <subhradwip01@gmail.com>
2022-02-16 12:50:20 +0530

    apending output in file in shell
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

---
+++ appending_output_to_end_of_the_text_file.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/bash
+
+# echo -e "Enter the file name: \c"
+# read file_name
+
+# if [ -f $file_name ]
+# then
+#     if [ -w $file_name ]
+#     then
+#         echo "Enter the text you wanne add(ctrl+d): "
+#         cat >> $file_name
+#     else 
+#         echo "$file_name doesn't have write permission"
+#     fi
+# else
+#     echo "$file_name not exist"
+# fi                

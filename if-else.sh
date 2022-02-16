e64a7b895a23feb5a6ea4168651c50909a4386b6
subhradwip01 <subhradwip01@gmail.com>
2022-02-16 12:48:10 +0530

    if statemnt in bash
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

---
+++ if-else.sh
@@ -0,0 +1,68 @@
+#! /usr/bin/bash
+
+############################################
+# if then - skeleton
+# if [ condition ]
+# then
+#     statement    
+# fi    
+
+
+# count=10
+
+# if [ $count -eq 9 ]
+# then 
+#     echo "Condition is true"
+# fi
+
+# if [ $count -gt 9 ]
+# then 
+#     echo "Condition is true"
+# fi
+
+# if [ $count -lt 9 ]
+# then 
+#     echo "Condition is true"
+# fi
+
+##########################################
+# if else - skeleton
+# if [ condition ]
+# then
+#     statement
+# else
+#    statement
+# fi    
+
+
+# num=10
+
+# if [[ $num < 9 ]]
+# then
+#     echo "Condition is true"
+# else
+#     echo "Condition is false"
+# fi        
+
+###############################################
+# if elif else - skeleton
+# if [ condition ]
+# then
+#     statement
+# elif 
+# then 
+#   statement  
+# else
+#    statement
+# fi 
+
+num=10
+# if (( $num < 7 ))
+# then
+#     echo "num<9"
+# elif [ $num -eq 10 ]
+# then
+#     echo "num==10"
+# else
+#     "Condition false" 
+# fi          

2d3f19613d33ace32b5954ac6f2c07d5c7037727
subhradwip01 <subhradwip01@gmail.com>
2022-02-16 12:48:59 +0530

    file testing opeartor in bash
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

---
+++ file_test_operator.sh
@@ -0,0 +1,41 @@
+#! /usr/bin/bash
+
+# For file
+# echo -e "Enter the file name: \c"
+# read file_name
+
+# # checking the existance of the file
+# if [ -e $file_name ]
+# then
+#     echo $file_name " exist"
+# else
+#     echo $file_name "not exist"
+# fi   
+
+# # if file exists and is a regular file.
+# if [ -f $file_name ]
+# then
+#     echo $file_name " regualr"
+# else
+#     echo $file_name "not regular"
+# fi 
+
+
+# # checking the emptyness of the file
+# if [ -s $file_name ]
+# then
+#     echo $file_name " empty"
+# else
+#     echo $file_name "not empty"
+# fi 
+
+# for Directory
+# echo -e "Enter the directory: \c"
+# read dir_name
+
+# # if [ -d $dir_name ]
+# then
+#     echo $dir_name " exist"
+# else
+#     echo $dir_name "not exist"
+# fi   

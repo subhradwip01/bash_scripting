005010300f1602ac3b896479b845d7b812ac49d4
subhradwip01 <subhradwip01@gmail.com>
2022-02-16 12:49:17 +0530

    and operator in abash
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

---
+++ and_operator.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/bash
+
+# age=18
+
+# if [ $age -ge 18 ] && [ $age -lt 30 ]
+# then
+#     echo "Valid age"
+# else
+# echo "Not valid"
+# fi
+
+
+
+# # anothetr way
+# if [ $age -ge 18 -a $age -lt 30 ]
+# then
+#     echo "Valid age"
+# else
+# echo "Not valid"
+# fi
+
+# # another way
+# if [[ $age -ge 18 && $age -lt 30 ]]
+# then
+#     echo "Valid age"
+# else
+# echo "Not valid"
+# fi

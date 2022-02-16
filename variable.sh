839c0f5f8ab1ec4562bd936c8c691fd816297615
subhradwip01 <subhradwip01@gmail.com>
2022-02-16 12:47:05 +0530

    bash varibales done
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

---
+++ variable.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/bash
+# Comment
+echo "Lets talk about variable"
+
+#Variacles
+
+#System variables: defined by system ==> usually used uppercase
+echo $BASH
+echo $BASH_VERSION
+echo $HOME
+echo $PWD
+
+
+#User defined variables: defined by user ==>  usually used smallcase 
+name=Mark
+age=19
+city=Kolkata
+echo Name: $name
+echo Age: $age
+echo city: $city
+
+_var=10
+echo Value: $_var

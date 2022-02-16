ea9cf3a21bde7e8d1d91f4c9c52001be701182b5
subhradwip01 <subhradwip01@gmail.com>
2022-02-16 12:49:36 +0530

    or operator in bash
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

---
+++ or_operator.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/bash
+
+# age=29
+
+# if [ $age -le 18 ] || [ $age -gt 30 ]
+# then
+#     echo "Valid age"
+# else
+# echo "Not valid"
+# fi
+
+# # another way
+# if [ $age -le 18 -o $age -gt 30 ]
+# then
+#     echo "Valid age"
+# else
+# echo "Not valid"
+# fi
+
+# # another way
+# if [[ $age -le 18 || $age -gt 30 ]]
+# then
+#     echo "Valid age"
+# else
+# echo "Not valid"
+# fi

c1480ed226d4d2bb6cdcbb04057678d2bd0e8854
subhradwip01 <subhradwip01@gmail.com>
2022-02-19 11:28:03 +0530

    break and continue in shell
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

---
+++ break_and_continue.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/bash
+
+# break==> to break the loop for a particular condition
+# for (( i=0; i<=10;i++))
+# do
+#     if [ $i -eq 5 ]
+#     then
+#         break
+#     fi
+#     echo $i
+# done    
+
+# contibue==> skip the particular iteration for a condition
+# for (( i=0; i<=10;i++))
+# do
+#     if [ $i -eq 5 ]
+#     then
+#         continue
+#     fi
+#     echo $i
+# done    
+ 

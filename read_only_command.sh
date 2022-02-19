cddb199a1c5ee953159b981edce106419651493f
subhradwip01 <subhradwip01@gmail.com>
2022-02-19 11:28:36 +0530

    read only command in shell
    Signed-off-by: subhradwip01 <subhradwip01@gmail.com>

---
+++ read_only_command.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/bash
+
+# readonly varibale =============================
+
+# var=31
+
+# readonly var # value of the varibale cannot be chnaged ( it is constatnt if any variable is readonly variable )
+
+# # var=49 # not posstible
+
+# echo "var=$var"
+
+
+# readonly function=========================
+
+# greeting () {
+#     echo "Hello!!!"
+# }
+
+# readonly -f greeting # this is a readonly function so it cannot be overridden
+
+# # greeting () {  # not possible
+# #     echo "Hi!!!"
+# # }
+
+# greeting
